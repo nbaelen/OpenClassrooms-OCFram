18148b877beb46e19f74d576445c22c5332e899f
nbaelen <n.baelen@gmail.com>
2018-07-22 21:05:42 +0200

    Ajout divers + ajout de la classe News

---
+++ lib/vendors/Entity/News.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Entity;
+
+use OCFram\Entity;
+
+class News extends Entity {
+    protected   $auteur,
+                $titre,
+                $contenu,
+                $dateAjout,
+                $dateModif;
+
+    const AUTEUR_INVALIDE = 1;
+    const TITRE_INVALIDE = 2;
+    const CONTENU_INVALIDE = 3;
+
+    // Mutateurs //
+
+    public function setAuteur($auteur) {
+        if (!is_string($auteur) || empty($auteur)) {
+            $this->erreurs[] = self::AUTEUR_INVALIDE;
+        }
+    }
+
+    public function setTitre($titre) {
+        if (!is_string($titre) || empty($titre)) {
+            $this->erreurs[] = self::TITRE_INVALIDE;
+        }
+    }
+
+    public function setContenu($contenu) {
+        if (!is_string($contenu) || empty($contenu)) {
+            $this->erreurs[] = self::CONTENU_INVALIDE;
+        }
+    }
+
+    public function setDateAjout(\DateTime $dateAjout) {
+        $this->dateAjout = $dateAjout;
+    }
+
+    public function setDateModif(\DateTime $dateModif) {
+        $this->dateModif = $dateModif;
+    }
+
+    // Accesseurs //
+
+    public function auteur() {
+        return $this->auteur;
+    }
+
+    public function titre() {
+        return $this->titre;
+    }
+
+    public function contenu() {
+        return $this->contenu;
+    }
+
+    public function dateAjout() {
+        return $this->dateAjout;
+    }
+
+    public function dateModif() {
+        return $this->dateModif;
+    }
+}
